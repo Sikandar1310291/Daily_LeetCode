fded0095dfd8aaec672dcfd7ac0e95880c93ca35
Muhammad Awais <awaisamjad31151@gmail.com>
2025-03-21 22:14:39 +0500

    Time: 11 ms (48.64%), Space: 28.4 MB (37.71%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -6,12 +6,9 @@ class Solution:
 
         for end in range(len(nums)):
             cur_sum += nums[end]
-            print(cur_sum)
 
             while cur_sum >= target:
                 size = min(size , end - start + 1)
-                print(size)
                 cur_sum -= nums[start]
-                print(cur_sum)
                 start += 1
         return size if size != len(nums) + 1 else 0
