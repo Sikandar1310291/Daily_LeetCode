ad64af391b35eabb1230f710e637b444bfa60c02
Muhammad Awais <awaisamjad31151@gmail.com>
2025-03-23 15:19:35 +0500

    Time: 0 ms (100%), Space: 18 MB (5.57%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,5 +5,5 @@ class Solution:
             if s[ptr_s] == t[ptr_t]:
                 ptr_s += 1
             ptr_t += 1
-        return ptr_s >= len(s)
         print(ptr_s >= len(s))
+        return ptr_s >= len(s)
